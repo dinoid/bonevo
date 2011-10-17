1d6023df014712b5e12399a11bed86fcb02e9378
Deniz Aydemir <deniz.aydemir@zerobuffer.com>
2011-10-18 00:07:04 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bonevo::Application.config.secret_token = 'ca3e17e9cee5a5693988ffb10c554ef5de3342713565ace056e8248450c9540a57f1a1ea9b36c077f58d1ff43ba45054a4545ecc1214cc014c5c7471e2758a5b'
